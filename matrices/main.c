d3f8bf1bd200770392d4cbbaac095fbf1d7e33a6
CsMargareta <72508599+csorema@users.noreply.github.com>
2025-02-27 13:22:21 +0100

    Add files via upload

---
+++ matrices/main.c
@@ -0,0 +1,42 @@
+#include "matrix.h"
+
+int main(int argc, char* argv[])
+{
+	float a[3][3] = {
+        { 1.0f, -2.0f,  3.0f},
+        { 5.0f, -3.0f,  0.0f},
+        {-2.0f,  1.0f, -4.0f}
+    };
+	float b[3][3];
+	float c[3][3];
+    float egysegmatrix[3][3];
+    float scalar = 2.5f;
+    float scmatrix[3][3] = {
+        { 1.0f, -2.0f,  3.0f},
+        { 5.0f, -3.0f,  0.0f},
+        {-2.0f,  1.0f, -4.0f}
+    };
+    
+
+    init_zero_matrix(b);
+    b[1][1] =  8.0f;
+    b[2][0] = -3.0f;
+    b[2][2] =  5.0f;
+
+    print_matrix(a);
+    print_matrix(b);
+    
+    add_matrices(a, b, c);
+
+    print_matrix(c);
+
+    init_identity_matrix(egysegmatrix);
+    print_matrix(egysegmatrix);
+
+    multiply_matrix_by_scalar(scmatrix, scalar);
+
+    print_matrix(scmatrix);
+    
+	return 0;
+}
+
